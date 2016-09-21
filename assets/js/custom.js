5a79a76abaa3c7ad841685292ae05f54d268e141
Justin S. Baiko <baiko@utexas.edu>
2016-09-20 21:22:49 -0500

    gallery for images tweak

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,10 +4,25 @@ $( document ).ready(function() {
 
     console.log( "ready!" );
 
-    $('.grid').magnificPopup({
-    delegate: '.grid-item', // child items selector, by clicking on it popup will open
-    type: 'image'
-    // other options
-  });
+    $(document).ready(function() {
+
+	$('.grid').magnificPopup({
+		delegate: 'div',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+});
 
 });
