7e6c2f2355f8959cbf4a44864a3f064efd415497
Justin S. Baiko <baiko@utexas.edu>
2016-09-20 19:38:39 -0500

    adding image gallery

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+
+$( document ).ready(function() {
+
+  $('.grid').magnificPopup({
+  delegate: '.grid-item', // child items selector, by clicking on it popup will open
+  type: 'image'
+  // other options
+});
+
+});
