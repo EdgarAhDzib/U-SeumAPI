7b745f4c71c2cfd124a95e484d8352d29ecff198
Justin S. Baiko <baiko@utexas.edu>
2016-09-28 11:34:11 -0500

    added signout functionality

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -95,17 +95,16 @@
         var email = $('#email-signup').val();
         var password = $('#password-signup').val();
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
+
         var errorCode = error.code;
         var errorMessage = error.message;
-        // [START_EXCLUDE]
+
         if (errorCode == 'auth/weak-password') {
           alert('The password is too weak.');
         } else {
           alert(errorMessage);
         }
         console.log(error);
-        // [END_EXCLUDE]
       });
         $.magnificPopup.close();
       });
@@ -117,21 +116,18 @@
         var email = $('#email-field').val();
         var password = $('#password-field').val();
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-          // Handle Errors here.
+
           var errorCode = error.code;
           var errorMessage = error.message;
-          // [START_EXCLUDE]
+
           if (errorCode === 'auth/wrong-password') {
             alert('Wrong password.');
           } else {
             alert(errorMessage);
           }
           console.log(error);
-          // [END_EXCLUDE]
+
         });
-
-
-
 
         $.magnificPopup.close();
       });
