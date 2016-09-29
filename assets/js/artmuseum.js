905af923895c37560e11ec75ab070fa133a13e7d
Justin S. Baiko <baiko@utexas.edu>
2016-09-28 22:54:24 -0500

    added heart for favorites

--- assets/js/artmuseum.js
+++ assets/js/artmuseum.js
@@ -210,7 +210,7 @@ $(document).ready( function(){
 	      // mfpResponse.data = $(mfpResponse.data).find('#some-element');
 	``
 	      // mfpResponse.data must be a String or a DOM (jQuery) element
-	      var HTML_part1 = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>captions</title></head><body><div class="ajax-text-and-image white-popup-block"><style>.ajax-text-and-image {max-width:800px; margin: 20px auto; background: #FFF; padding: 0; line-height: 0;}.ajcol {width: 50%; float:left;}.ajcol img {width: 100%; height: auto;}@media all and (max-width:30em) {.ajcol {width: 100%;float:none;}}</style><div class="ajcol"><img style="object-fit: contain;" src=';
+	      var HTML_part1 = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>captions</title><script src="assets/js/captions.js"></script></head><body><div class="ajax-text-and-image white-popup-block"><style>.ajax-text-and-image {max-width:800px; margin: 20px auto; background: #FFF; padding: 0; line-height: 0;}.ajcol {width: 50%; float:left;}.ajcol img {width: 100%; height: auto;}@media all and (max-width:30em) {.ajcol {width: 100%;float:none;}}</style><div class="ajcol"><img style="object-fit: contain;" src=';
 	      var HTML_part2 = '></div><div class="ajcol" style="line-height: 120%;"><div style="padding: 1em"><p><i>'; //Title
 	      var HTML_part3 = '</i><sub> by '; //artist
 	      var HTML_part4 = '</sub></p><p>'; //culture
@@ -225,7 +225,7 @@ $(document).ready( function(){
 	      if (Collection_info != "") {
 	      	HTML_part10 += '<p>' + Collection_info + '</p><p><sub> collection</sub></p>';
 	      }
-	      var HTML_end =  '</div></div><div style="clear:both; line-height: 0;"></div></div></body></html>';
+	      var HTML_end =  '</div><div class="ui massive heart rating" id="heart-placement" data-rating="0" data-max-rating="1"> </div></div><div style="clear:both; line-height: 0;"></div></div></body></html>';
 // Add the Collection to this code
 	      var title = Title;
 	      var artist = Artist;
