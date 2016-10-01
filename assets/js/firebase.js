bb8ace5bfdea4c0fa6bfe89b43a730499eb935a9
Aaron Lee <worldedit05@gmail.com>
2016-10-01 02:19:47 -0500

    Bug fixes

---
+++ assets/js/firebase.js
@@ -0,0 +1,113 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCFvZM-io7AWOKZLwaXN13laMjamcCXsiY",
+    authDomain: "u-seum.firebaseapp.com",
+    databaseURL: "https://u-seum.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "870561197845"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
+var database;
+var array;
+
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture: imageUrl,
+        firstName: fName,
+        joinDate: jDate,
+        shortBio: sBio,
+        timeSpent: tSpent,
+        viewCount: vCount,
+        longBio: lBio,
+        friendCount: fCount,
+        favCount: favCount
+    });
+}
+
+
+function signOut() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+    }, function(error) {
+        // An error happened.
+    });
+}
+
+$(function() {
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("is this working. plz let it be");
+            userId = user.uid;
+
+            console.log("signed in");
+            $('#sign-in').html('Sign out');
+            $('#sign-in').click(function() {
+                signOut();
+                $.magnificPopup.close();
+            });
+
+
+
+        } else {
+            // No user is signed in.
+            $('#sign-in').html('Sign in');
+            console.log("not signed in");
+        }
+    });
+
+
+    $('.popup-modal').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#username',
+        modal: true
+    });
+    $('#signup-button').click(function() {
+        var email = $('#email-signup').val();
+        var password = $('#password-signup').val();
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
+        $.magnificPopup.close();
+    });
+    $(document).on('click', '.popup-modal-dismiss', function(e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+    });
+
+
+    $('#login-button').click(function() {
+        var email = $('#email-field').val();
+        var password = $('#password-field').val();
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+
+        });
+
+        $.magnificPopup.close();
+    });
+
+});
