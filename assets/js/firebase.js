b39afeff7641ea62f1f570e2584b0807b7db9556
Justin S. Baiko <baiko@utexas.edu>
2016-10-01 00:34:56 -0500

    new branch

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -48,21 +48,7 @@
 
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          /*
-          // Retrieves favorite pictures from saved list & adds a new one. Basically everytime we would hit the like button it would call this function.
-          database = firebase.database().ref('users/' + user.uid + '/favoritePics');
-          database.on('value', function(snapshot) {
-            array = snapshot.val();
-            console.log("array is: " + array);
-            //return array;
-          });
-
-          var pics = [];
-          console.log("pics");
-          userID = user.uid;
-          pics.push("http://lh4.ggpht.com/NwCWmjro4h__Ord5RqicIJsJbTY104UditPHR-swB9a7pQRt67KfneX_tBEazLnkNGsWqCvfsZam8Pxj1Ixiqbne7Q=s0");
-          addPicture(user,pics);
-          */
+          
 
           console.log("signed in");
           $('#sign-in').html('Sign out');
